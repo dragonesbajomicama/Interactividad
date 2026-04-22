195a38ad1178e34162f0bcaab67d443907a2b35a
dragonesbajomicama <amilanes@centro.edu.mx>
2026-04-21 20:17:12 -0600

    esquinas

--- js/cursor.js
+++ js/cursor.js
@@ -1,29 +1,71 @@
 let size = 42;
-let fill_color = "black";
-let stroke_color = "white";
+let fillColor = "black";
+let strokeColor = "white";
+
 let x = 0;
 let y = 0;
-friction = 0.58;
+let friction = 0.58;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(0);
-    //frameRate(60);
-    
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
-    background(0, 35);
-    updateCursor();
-   
+  background(255, 255, 255, 3);
+  updateCursor();
 }
 
 function updateCursor() {
-    
-    stroke(stroke_color);
+  x += (mouseX - x) * friction;
+  y += (mouseY - y) * friction;
 
-    x+= (mouseX - x) * friction;
-    y+= (mouseY - y) * friction;
-    ellipse(x, y, size, size);    
+  // Detectar esquina según posición
+  let corner = "";
+
+  if (x < width / 2 && y < height / 2) {
+    corner = "1"; // arriba izquierda
+  } else if (x >= width / 2 && y < height / 2) {
+    corner = "2"; // arriba derecha
+  } else if (x < width / 2 && y >= height / 2) {
+    corner = "3"; // abajo izquierda
+  } else {
+    corner = "4"; // abajo derecha
+  }
+
+  // Switch case para color
+  switch (corner) {
+    case "1":
+      fillColor = "red";
+      size = 50;
+      ellipse(x, y, size, size);
+      break;
+
+    case "2":
+      fillColor = "blue";
+      rect(x, y, size, size);
+      size = 30;
+      break;
+
+    case "3":
+      fillColor = "green";
+      size = 70;
+      rect(x, y, size, size);
+      break;
+
+    case "4":
+      fillColor = "yellow";
+      size = 90;
+      ellipse(x, y, size, size);
+      break;
+
+    default:
+      fillColor = "white";
+      size = 42;
+      break;
+  }
+
+  fill(fillColor);
   
 }
+
